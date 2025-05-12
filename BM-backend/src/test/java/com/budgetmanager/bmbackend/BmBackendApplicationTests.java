45246a6b7f82e1863d916c567c829bf118303c62
oussamasine07 <sineoussama@gmail.com>
2025-05-12 17:35:03 +0100

    add backend spring boot

---
+++ BM-backend/src/test/java/com/budgetmanager/bmbackend/BmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetmanager.bmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
