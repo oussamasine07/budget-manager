396b04e2adfba4a8e3664de8037e74667e460d7f
ANASTAD2000 <anasbaclibr@gmai.com>
2025-05-14 15:58:06 +0100

    add methods crud for catigiries

--- BM-backend/src/main/java/com/budgetmanager/bmbackend/controller/CategoryController.java
+++ BM-backend/src/main/java/com/budgetmanager/bmbackend/controller/CategoryController.java
@@ -1,7 +1,13 @@
 package com.budgetmanager.bmbackend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.budgetmanager.bmbackend.model.Category;
+import com.budgetmanager.bmbackend.repository.CateogoryRepository;
+import com.budgetmanager.bmbackend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -9,4 +15,37 @@ public class CategoryController {
     // inject Category service
 
     // all controller methods (CRUD)
+
+
+    @Autowired
+    private CateogoryRepository cateogoryRepository;
+//get all categieis
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return cateogoryRepository.findAll();
+    }
+//    save category
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return cateogoryRepository.save(category);
+    }
+
+//    updat category
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable long id, @RequestBody Category category) {
+        return (ResponseEntity<Category>) cateogoryRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+//    delet category
+    @DeleteMapping("{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable long id) {
+        if (cateogoryRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        cateogoryRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
