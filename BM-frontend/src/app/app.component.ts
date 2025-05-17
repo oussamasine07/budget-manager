482505ec857d3bdd0a64d2f7ca0fc4ce490776a4
oussamasine07 <sineoussama@gmail.com>
2025-05-17 14:11:36 +0100

    make budget header layout

--- BM-frontend/src/app/app.component.ts
+++ BM-frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BudgetHeaderComponent} from './components/budget-header/budget-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BudgetHeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ BM-frontend/src/app/components/budget-header/budget-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-header',
+  imports: [],
+  templateUrl: './budget-header.component.html',
+  styleUrl: './budget-header.component.css'
+})
+export class BudgetHeaderComponent {
+
+}
